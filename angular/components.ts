5364ea9f17473aec1badc5d6f565ab362d6096ba
Ale Vilar <alevilar@gmail.com>
2023-04-09 19:35:35 -0300

    package

--- angular/components.ts
+++ angular/components.ts
@@ -6,7 +6,40 @@ import { ProxyCmp, proxyOutputs } from './angular-component-lib/utils';
 
 import type { Components } from 'input-file-from-webcam/components';
 
+import { defineCustomElement as defineInputFaceApiWebcam } from 'input-file-from-webcam/components/input-face-api-webcam.js';
 import { defineCustomElement as defineInputFileFromWebcam } from 'input-file-from-webcam/components/input-file-from-webcam.js';
+@ProxyCmp({
+  defineCustomElementFn: defineInputFaceApiWebcam,
+  inputs: ['height', 'photoPicMinValue', 'width'],
+  methods: ['stopDetection', 'startDetection']
+})
+@Component({
+  selector: 'input-face-api-webcam',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: '<ng-content></ng-content>',
+  // eslint-disable-next-line @angular-eslint/no-inputs-metadata-property
+  inputs: ['height', 'photoPicMinValue', 'width'],
+})
+export class InputFaceApiWebcam {
+  protected el: HTMLElement;
+  constructor(c: ChangeDetectorRef, r: ElementRef, protected z: NgZone) {
+    c.detach();
+    this.el = r.nativeElement;
+    proxyOutputs(this, this.el, ['faceDetected', 'faceMinValueError']);
+  }
+}
+
+
+import type { FaceDetection as IInputFaceApiWebcamFaceDetection } from 'input-file-from-webcam/components';
+
+export declare interface InputFaceApiWebcam extends Components.InputFaceApiWebcam {
+
+  faceDetected: EventEmitter<CustomEvent<Blob>>;
+
+  faceMinValueError: EventEmitter<CustomEvent<IInputFaceApiWebcamFaceDetection>>;
+}
+
+
 @ProxyCmp({
   defineCustomElementFn: defineInputFileFromWebcam,
   inputs: ['drawImageCb', 'facingMode', 'height', 'width'],
