40517151a0322d997c16360a5995238ba84c323b
Ale Vilar <alevilar@gmail.com>
2025-08-22 12:33:28 -0300

    feat: update package version to 2.0.0 and export additional types and interfaces in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
 export * from './components';
+
+// Export types and interfaces used by components
+export { CameraDirection } from './utils/camera.service';
+export type { LabeledDescriptorsArray } from './components/input-face-api-webcam/TrainedModel';
+export type { FaceDetectionError, iFaceDetected } from './components/input-face-api-webcam/input-face-api-webcam';
+export type { DetectionImg } from './utils/facepi.service';
+export type { WebcamError } from './components/input-file-from-webcam/input-file-from-webcam';
+export type { InputScanData } from './components/input-scan-reader/input-scan-reader.types';
